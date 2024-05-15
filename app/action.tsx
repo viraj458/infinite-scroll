7a231330123b4358814866aa41f3ad70c69c9640
viraj458 <virajmalaka94@gmail.com>
2024-05-15 14:33:45 +0530

    Infinite scroll

---
+++ app/action.tsx
@@ -0,0 +1,15 @@
+'use server';
+
+import AnimeCard, { AnimeProp } from '@/components/AnimeCard';
+
+export const fetchAnime = async (page: number) => {
+  const respose = await fetch(
+    `https://shikimori.one/api/animes?page=${page}&limit=8&order=popularity`,
+  );
+
+  const data = await respose.json();
+
+  return data.map((item: AnimeProp, index: number) => (
+    <AnimeCard key={item.id} anime={item} index={index} />
+  ));
+};
